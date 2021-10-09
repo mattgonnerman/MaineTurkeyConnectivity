c74f0d47c3e657d6ba9edbc52a785b052bce1bc3
Matt Gonnerman <matthew.gonnerman@maine.edu>
2021-10-09 11:32:03 -0400

    update

--- MTC - WMD Connect File Cleanup.R
+++ MTC - WMD Connect File Cleanup.R
@@ -12,24 +12,36 @@ df <- cleanup %>%
 
 head(df)
 
-write.table(df, "E:/Maine Drive/Analysis/Dissertation Backup/TurkeyConnectivity/WMDConnectSimTracks.csv", row.names = F,
-          append = T, sep = ",", col.names = !file.exists("E:/Maine Drive/Analysis/Dissertation Backup/TurkeyConnectivity/WMDConnectSimTracks.csv"))
+write.table(df, "E:/Maine Drive/Analysis/Dissertation Backup/TurkeyConnectivity/WMDConnectSimTracks_part1.csv", row.names = F,
+          append = T, sep = ",", col.names = !file.exists("E:/Maine Drive/Analysis/Dissertation Backup/TurkeyConnectivity/WMDConnectSimTracks_part1.csv"))
 
 # Delete all the files so you don't duplicate entries
 sapply(cleanup, unlink)
 
-df.merged <- read.csv("E:/Maine Drive/Analysis/Dissertation Backup/TurkeyConnectivity/WMDConnectSimTracks.csv")
+# df.merged <- read.csv("E:/Maine Drive/Analysis/Dissertation Backup/TurkeyConnectivity/WMDConnectSimTracks.csv")
+# 
+# #How many simulations have been completed
+# nrow(df.merged)
+# length(unique(df.merged$BirdID))
+# 
+# 
+# #How Many Birds Dispersed
+# df.merged %>%
+#   group_by(BirdID) %>% 
+#   summarize(Total = n()) %>%
+#   ungroup() %>%
+#   mutate(Dispersed = ifelse(Total == 2, 0, 1)) %>%
+#   group_by(Dispersed) %>%
+#   summarize(Total = n())
 
-#How many simulations have been completed
-nrow(df.merged)
-length(unique(df.merged$BirdID))
+df.merged.1 <- read.csv("E:/Maine Drive/Analysis/Dissertation Backup/TurkeyConnectivity/WMDConnectSimTracks_part1.csv")
+df.merged.2 <- read.csv("E:/Maine Drive/Analysis/Dissertation Backup/TurkeyConnectivity/WMDConnectSimTracks_part2.csv")
+df.merged.3 <- read.csv("E:/Maine Drive/Analysis/Dissertation Backup/TurkeyConnectivity/WMDConnectSimTracks_part3.csv")
+df.merged.4 <- read.csv("E:/Maine Drive/Analysis/Dissertation Backup/TurkeyConnectivity/WMDConnectSimTracks_part4.csv")
 
+df.merged <- do.call("rbind", list(df.merged.1, df.merged.2, df.merged.3, df.merged.4))
 
-#How Many Birds Dispersed
-df.merged %>%
-  group_by(BirdID) %>% 
-  summarize(Total = n()) %>%
-  ungroup() %>%
-  mutate(Dispersed = ifelse(Total == 2, 0, 1)) %>%
-  group_by(Dispersed) %>%
-  summarize(Total = n())
+df.comp <- unique(df.merged$BirdID)
+length(df.comp)
+
+save(df.comp, file = "E:/Maine Drive/Analysis/Dissertation Backup/TurkeyConnectivity/CompletedSims.RData")
